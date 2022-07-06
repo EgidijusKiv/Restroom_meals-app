7f004f16e4223eb09910b17556636f2599e821dc
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-06 09:10:11 +0300

    "Back" button correction

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback, useContext } from 'react'
 import './HomePage.scss'
 import '../RandomMeal/RandomMeal.scss'
 import { myContext } from "../context/context";
-import {HiChevronDoubleLeft} from 'react-icons/hi'
+import { HiChevronDoubleLeft } from 'react-icons/hi'
 
 const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -13,7 +13,7 @@ const HomePage = () => {
     fetchHomePageMeals(searchTerm);
   }, [searchTerm, fetchHomePageMeals]);
 
-  return ( !imgClick ?
+  return (!imgClick ?
     <div className='home'>
       <div className='home-search'>
         <input
@@ -27,38 +27,38 @@ const HomePage = () => {
       <div className='home-grid'>
         {meals ? (
           meals.map((meal) => <div className='home-meal' key={meal.idMeal}>
-            <img 
-            onClick={() => {
-fetchMealById(meal.idMeal);
-setImgClick(true);
-            } } 
-            src={meal.strMealThumb} 
-            alt='#'/>
+            <img
+              onClick={() => {
+                fetchMealById(meal.idMeal);
+                setImgClick(true);
+              }}
+              src={meal.strMealThumb}
+              alt='#' />
             <h4>{meal.strMeal}</h4>
           </div>)
-          ) : (
-            <h2>No Meals found! Try another word...</h2>
-          )}
+        ) : (
+          <h2>No Meals found! Try another word...</h2>
+        )}
       </div>
 
     </div>
     :
     <div className='random'>
-    {mealsById.map((meal) => (
-      <div key={meal.idMeal} className='random-grid'>
-        <div className='random-grid-controls'>
-        <button onClick={()=> setImgClick(false)}><HiChevronDoubleLeft/> Back</button>
-          <h4>{meal.strMeal}</h4>
-          <img src={meal.strMealThumb} alt="#" />
-        </div>
+      {mealsById.map((meal) => (
+        <div key={meal.idMeal} className='random-grid'>
+          <button className='back' onClick={() => setImgClick(false)}><HiChevronDoubleLeft /> Back</button>
+          <div className='random-grid-controls'>
+            <h4>{meal.strMeal}</h4>
+            <img src={meal.strMealThumb} alt="#" />
+          </div>
 
-        <div className='random-grid-instructions'>
-          <h4>Instructions</h4>
-          <p>{meal.strInstructions}</p>
+          <div className='random-grid-instructions'>
+            <h4>Instructions</h4>
+            <p>{meal.strInstructions}</p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
+      ))}
+    </div>
   )
 }
 
